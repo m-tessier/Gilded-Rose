3fdab3b2d0bd6b34972f8cafc13a2c62f20bb627
m-tessier <morgane.tessier22@gmail.com>
2021-05-26 15:28:12 +0200

    add conjured

--- src/js/Shop.js
+++ src/js/Shop.js
@@ -25,6 +25,14 @@ class Shop {
             item.sellIn < 0 ? item.quality = 0 : null;
           }         
           break;
+        case String(item.name.match(/^Conjured.*/)) :
+          item.quality > 1 ? item.quality -= 2 : null;
+          item.quality === 1 ? item.quality -= 1 : null;
+          if (item.sellIn < 0) {
+            item.quality > 1 ? item.quality -= 2 : null;
+            item.quality === 1 ? item.quality -= 1 : null;
+          }
+          break;
         default :
         item.quality > 0 ? item.quality -= 1 : null;
         item.sellIn < 0 && item.quality > 0 ? item.quality -= 1 : null;
